5e5972447086881eebfdc592eb716e55df2452b0
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-25 10:12:46 +0100

    [add] Appslider

--- src/components/AppSliderBadge.vue
+++ src/components/AppSliderBadge.vue
@@ -1,21 +1,104 @@
 <script>
 export default {
-    name:'AppSliderBadge'
+    name:'AppSliderBadge',
+    data(){
+      return{
+        items:[
+                {
+                  cat:'gadgets',
+                  href: 'www.google.com'
+                },
+                {
+                  cat:'photography',
+                  href: '#'
+                },
+                {
+                  cat:'lifestyle',
+                  href: '#'
+                },
+                {
+                  cat:'fashion',
+                  href: '#'
+                },
+                {
+                  cat:'recipes',
+                  href: '#'
+                },
+                {
+                  cat:'travel',
+                  href: '#'
+                },
+              ]
+      }
+    }
 }
 </script>
 
 <template>
+
   <section>
     <div class="slider">
+      <div class="container h-100 d-flex justify-content-center ">
+        <div class="row h-100 d-flex align-items-center">
 
+          <div class="col">
+
+            <div class="box-badge d-flex flex-wrap justify-content-center">
+              <div v-for="item in items" :key="item" class="badge">{{item.cat}}</div>
+            </div>
+
+            <div class="prev">
+              <i class="fa-solid fa-chevron-left"></i>
+            </div>
+
+            <div class="next">
+              <i class="fa-solid fa-chevron-right"></i>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   </section>
+
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/vars' as *;
+@use '../styles/partials/mixin' as *;
 .slider{
-    height: 150px;
+    min-height: 150px;
     background: $bg-main-color;
+    .container{
+      min-height: 150px;
+      .row{
+        min-height: 150px;
+      }
+    }
+  .col{
+    position: relative;
+    .badge{
+      @include badge('big');
+       margin: 10px 17.5px;
+      max-width: 185px;
+      min-width: 185px;
+      display: flex;
+      justify-content: center;
+    }
+    .prev,
+    .next{
+      position: absolute;
+      transform: translateY(-50%);
+    }
+    .prev{
+      top: 50%;
+      left: 0;
+    }
+    .next{
+      top: 50%;
+      right: 0;
+    }
+  }
 }
 </style>
